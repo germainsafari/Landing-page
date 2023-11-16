7bb965635696d63cc96d8cb13a2a93001d50c5ad
germainsafari <GG@ee10725997111>
2023-11-16 17:08:56 +0100

    message

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 function Header() {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif",
+  }
   return (
     <header className="flex items-center justify-between">
+
       <h1 className="text-3xl font-bold">SIMPLE / BEAUTIFUL LANDING PAGE</h1>
+      {/* <h1 style={{backgroundColor:"lightblue"}}>Improvise</h1> */}
+      <h2 style={myStyle}></h2>
       <nav className="flex space-x-4">
         <a href="#">HOME</a>
         <a href="#">ABOUT</a>
